be0ad86e7bbac9ef0e13f493a75e86b655f21458
sarori <salo3678@gmail.com>
2020-01-12 12:09:19 +0900

    .

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-	render() {
+	constructor(props) {
+		super(props);
+		//This is the only time we do direct assignment to this.state
+		this.state = { lat: null };
+
 		window.navigator.geolocation.getCurrentPosition(
-			position => console.log(position),
+			position => {
+				//we called setstate!
+				this.setState({ lat: position.coords.latitude });
+			},
 			err => console.log(err)
 		);
-		return <div>Latitude: </div>;
+	}
+	render() {
+		return <div>Latitude: {this.state.lat} </div>;
 	}
 }
 
